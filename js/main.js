2edcd731c1264930b9bb770a11d9b889c346fedf
An Le <truonganletk@gmail.com>
2022-12-29 16:13:56 +0300

    header responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let items = document.querySelectorAll('.carousel .carousel-item')
-console.log(items);
+// console.log(items);
 items.forEach((el) => {
     const minPerSlide = 3
     let next = el.nextElementSibling
@@ -11,5 +11,34 @@ items.forEach((el) => {
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
         next = next.nextElementSibling
+    }
+})
+
+const collapseiconleft = document.getElementById('collapse-icon-left');
+collapseiconleft.addEventListener('click',()=>{
+    const navleft = document.getElementById('navleft');
+    navleft.classList.toggle('navleft-lg');
+    if (navleft.classList.contains('navleft-lg')) {
+        const navright = document.getElementById('navright');
+        navright.classList.remove('navright-lg');
+    }
+})
+
+const collapseiconright = document.getElementById('collapse-icon-right');
+collapseiconright.addEventListener('click',()=>{
+    const navright = document.getElementById('navright');
+    navright.classList.toggle('navright-lg');
+    if (navright.classList.contains('navright-lg')) {
+        const navleft = document.getElementById('navleft');
+        navleft.classList.remove('navleft-lg');
+    }
+})
+
+document.addEventListener('scroll',()=>{
+    const header = document.getElementById('header');
+    if (window.scrollY>134){
+        header.classList.add('header-fixed');
+    } else {
+        header.classList.remove('header-fixed');
     }
 })
