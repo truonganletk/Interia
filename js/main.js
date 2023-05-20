207231e9f0f0bd8d579edc0b5c220f36d50c227d
An Le <truonganletk@gmail.com>
2023-05-20 15:37:45 +0300

    budget responsive

--- js/main.js
+++ js/main.js
@@ -1,44 +1,129 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
+let items = document.querySelectorAll(".carousel .carousel-item");
 // console.log(items);
 items.forEach((el) => {
-    const minPerSlide = 3
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+  const minPerSlide = 3;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
     }
-})
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
 
-const collapseiconleft = document.getElementById('collapse-icon-left');
-collapseiconleft.addEventListener('click',()=>{
-    const navleft = document.getElementById('navleft');
-    navleft.classList.toggle('navleft-lg');
-    if (navleft.classList.contains('navleft-lg')) {
-        const navright = document.getElementById('navright');
-        navright.classList.remove('navright-lg');
-    }
-})
+const collapseiconleft = document.getElementById("collapse-icon-left");
+collapseiconleft.addEventListener("click", () => {
+  const navleft = document.getElementById("navleft");
+  navleft.classList.toggle("navleft-lg");
+  if (navleft.classList.contains("navleft-lg")) {
+    const navright = document.getElementById("navright");
+    navright.classList.remove("navright-lg");
+  }
+});
 
-const collapseiconright = document.getElementById('collapse-icon-right');
-collapseiconright.addEventListener('click',()=>{
-    const navright = document.getElementById('navright');
-    navright.classList.toggle('navright-lg');
-    if (navright.classList.contains('navright-lg')) {
-        const navleft = document.getElementById('navleft');
-        navleft.classList.remove('navleft-lg');
-    }
-})
+const collapseiconright = document.getElementById("collapse-icon-right");
+collapseiconright.addEventListener("click", () => {
+  const navright = document.getElementById("navright");
+  navright.classList.toggle("navright-lg");
+  if (navright.classList.contains("navright-lg")) {
+    const navleft = document.getElementById("navleft");
+    navleft.classList.remove("navleft-lg");
+  }
+});
 
-document.addEventListener('scroll',()=>{
-    const header = document.getElementById('header');
-    if (window.scrollY>134){
-        header.classList.add('header-fixed');
+document.addEventListener("scroll", () => {
+  const header = document.getElementById("header");
+  if (window.scrollY > 134) {
+    header.classList.add("header-fixed");
+  } else {
+    header.classList.remove("header-fixed");
+  }
+});
+
+// custom select
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("budget-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      /*when an item is clicked, update the original select box,
+        and the selected item:*/
+      var y, i, k, s, h, sl, yl;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      sl = s.length;
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < sl; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          yl = y.length;
+          for (k = 0; k < yl; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x,
+    y,
+    i,
+    xl,
+    yl,
+    arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
     } else {
-        header.classList.remove('header-fixed');
+      y[i].classList.remove("select-arrow-active");
     }
-})
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
